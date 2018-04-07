20e871b8533f755583078d59ed9fdbe823106faf
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-04-07 16:05:58 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,59 @@
+import discord
+import os
+import io
+import traceback
+import sys
+import time
+import datetime
+import asyncio
+import random
+import aiohttp
+import random
+import textwrap
+import inspect
+from contextlib import redirect_stdout
+from discord.ext import commands
+import json
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('_'),description="Remixbot but not...\n\nHelp Commands",owner_id=250674147980607488)
+
+
+
+@bot.event
+async def on_ready():
+    print('Bot is online, and ready to ROLL!')
+    while True:
+        await bot.change_presence(activity=discord.Game(name=f"with {len(bot.guilds)} servers!"))
+        await asyncio.sleep(10)
+        await bot.change_presence(activity=discord.Game(name="**help"))
+        await asyncio.sleep(10)
+        
+        
+        
+@bot.command()
+async def ping(ctx):
+    """Get the bot's Websocket latency."""
+    color = discord.Color(value=0x00ff00)
+    em = discord.Embed(color=color, title=':ping_pong: **Pong!**')
+    em.description = f"{bot.latency * 1000:.4f} ms"
+    await ctx.send(embed=em)
+    
+    
+    
+    
+    
+@bot.command()
+async def say(ctx, *, message:str):
+    """Speak as me!"""
+    await ctx.message.delete()
+    await ctx.send(message)    
+    
+    
+    
+@bot.command()
+async def gay(ctx):
+await.ctx send ("**https://www.youtube.com/watch?v=noQXojwExRA**")
+
+
+if not os.environ.get('TOKEN'):
+   print("no token found REEEE!")
+bot.run(os.environ.get('TOKEN').strip('"'))
